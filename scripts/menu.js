85ba14c0fac7b40d36503abc491feeb68e083baa
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-10-25 21:48:19 -0300

    corrige css

---
+++ scripts/menu.js
@@ -0,0 +1,14 @@
+function openMenu() {
+    const menu = document.querySelector('.header-container-mobile')
+    console.log('aaaaaaa')
+    console.log(menu)
+
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'
+
+    } else {
+        menu.style.display = 'none'
+
+    }
+
+}
